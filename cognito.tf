184c80121383f8b913862779ad7abcabcacb5805
bahi1023 <bebomohammed904@gmail.com>
2026-02-09 18:13:11 +0200

    Feat: Enable Cognito Auth & Enforce JWT on API Gateway

--- cognito.tf
+++ cognito.tf
@@ -1,54 +1,54 @@
-# resource "aws_cognito_user_pool" "main" {
-#   name = "${var.project_name}-user-pool-${var.environment}"
-#
-#   password_policy {
-#     minimum_length    = 8
-#     require_lowercase = true
-#     require_numbers   = true
-#     require_symbols   = true
-#     require_uppercase = true
-#   }
-#
-#   username_attributes      = ["email"]
-#   auto_verified_attributes = ["email"]
-#
-#   tags = {
-#     Project     = var.project_name
-#     Environment = var.environment
-#   }
-#
-#   admin_create_user_config {
-#     allow_admin_create_user_only = false
-#   }
-# }
-#
-#
-#
-# resource "aws_cognito_user_pool_client" "client" {
-#   name = "${var.project_name}-client"
-#
-#   user_pool_id = aws_cognito_user_pool.main.id
-#
-#   generate_secret = true
-#
-#   callback_urls = ["${var.nlb_dns_name}/oauth2/callback"]
-#   logout_urls   = ["${var.nlb_dns_name}/"]
-#
-#   supported_identity_providers = ["COGNITO"]
-#
-#   allowed_oauth_flows_user_pool_client = true
-#   allowed_oauth_flows                  = ["code", "implicit"]
-#   allowed_oauth_scopes                 = ["email", "openid", "profile"]
-#
-#   explicit_auth_flows = [
-#     "ALLOW_USER_PASSWORD_AUTH",
-#     "ALLOW_REFRESH_TOKEN_AUTH",
-#     "ALLOW_USER_SRP_AUTH",
-#     "ALLOW_ADMIN_USER_PASSWORD_AUTH"
-#   ]
-# }
-#
-# resource "aws_cognito_user_pool_domain" "main" {
-#   domain       = "${var.project_name}-${var.environment}-auth"
-#   user_pool_id = aws_cognito_user_pool.main.id
-# }
+resource "aws_cognito_user_pool" "main" {
+  name = "${var.project_name}-user-pool-${var.environment}"
+
+  password_policy {
+    minimum_length    = 8
+    require_lowercase = true
+    require_numbers   = true
+    require_symbols   = true
+    require_uppercase = true
+  }
+
+  username_attributes      = ["email"]
+  auto_verified_attributes = ["email"]
+
+  tags = {
+    Project     = var.project_name
+    Environment = var.environment
+  }
+
+  admin_create_user_config {
+    allow_admin_create_user_only = false
+  }
+}
+
+
+
+resource "aws_cognito_user_pool_client" "client" {
+  name = "${var.project_name}-client"
+
+  user_pool_id = aws_cognito_user_pool.main.id
+
+  generate_secret = true
+
+  callback_urls = ["${var.nlb_dns_name}/oauth2/callback"]
+  logout_urls   = ["${var.nlb_dns_name}/"]
+
+  supported_identity_providers = ["COGNITO"]
+
+  allowed_oauth_flows_user_pool_client = true
+  allowed_oauth_flows                  = ["code", "implicit"]
+  allowed_oauth_scopes                 = ["email", "openid", "profile"]
+
+  explicit_auth_flows = [
+    "ALLOW_USER_PASSWORD_AUTH",
+    "ALLOW_REFRESH_TOKEN_AUTH",
+    "ALLOW_USER_SRP_AUTH",
+    "ALLOW_ADMIN_USER_PASSWORD_AUTH"
+  ]
+}
+
+resource "aws_cognito_user_pool_domain" "main" {
+  domain       = "${var.project_name}-${var.environment}-auth"
+  user_pool_id = aws_cognito_user_pool.main.id
+}
