bfd097837ed21b843418cabda9f3f92dd143c520
bahi1023 <bebomohammed904@gmail.com>
2026-02-10 12:54:09 +0200

    final_yarab

--- output.tf
+++ output.tf
@@ -44,3 +44,8 @@ output "nlb_dns_name" {
   description = "The DNS name of the Network Load Balancer"
   value       = var.nlb_dns_name
 }
+
+output "cognito_login_url" {
+  description = "Direct link to the Cognito Hosted UI Login page"
+  value       = "https://${aws_cognito_user_pool_domain.main.domain}.auth.${var.aws_region}.amazoncognito.com/login?client_id=${aws_cognito_user_pool_client.client.id}&response_type=code&scope=email+openid+profile&redirect_uri=${aws_apigatewayv2_api.main.api_endpoint}/oauth2/callback"
+}
